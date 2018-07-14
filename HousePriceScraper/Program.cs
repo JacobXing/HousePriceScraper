159486c3e29d8a230b13f5edd8c0c54a46809e8c
JacobXing <37539965+JacobXing@users.noreply.github.com>
2018-07-14 19:07:18 +1000

    HouseSpider

---
+++ HousePriceScraper/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using CsvHelper;
+
+
+namespace HousePriceScraper
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // --csv=filepath
+
+            Regex csv = new Regex(@"^\-\-csv=");
+
+            var argCsvPath = args.FirstOrDefault(arg => csv.IsMatch(arg));
+
+            var pathCsv = csv.Replace(argCsvPath, "");
+
+            Spider spider = new Spider();
+
+            int lineNumber = 0;
+            string lineNumberPath = $"{AppContext.BaseDirectory}/line.txt";
+
+            if (File.Exists(lineNumberPath))
+            {
+                lineNumber = int.Parse(File.ReadAllText(lineNumberPath));
+
+                Console.WriteLine($"Resumed from {lineNumber}");
+            }
+
+            int index = 0;
+
+            using (StreamReader csvStreamReader = new StreamReader(pathCsv))
+            {
+                using (CsvReader csvReader = new CsvReader(csvStreamReader))
+                {
+                    csvReader.Configuration.BadDataFound = null;
+                    // csvReader.Read();
+
+                    while (csvReader.Read())
+                    {
+                        if (index < lineNumber)
+                        {
+                            var row = csvReader.GetRecord<dynamic>();
+                            var dict = row as IDictionary<string, object>;
+                        }
+                        else
+                        {
+                            var row = csvReader.GetRecord<dynamic>();
+                            var dict = row as IDictionary<string, object>;
+
+                            var prop = new Property();
+
+                            prop.UnitNumber = dict["UNIT NUMBER"] as string;
+                            prop.HouseNumber = dict["HOUSE NUMBER"] as string;
+                            prop.StreetName = dict["STREET NAME"] as string;
+                            prop.StreetType = dict["STREET TYPE"] as string;
+                            prop.StreetSuffix = dict["STREET SUFFIX"] as string;
+                            prop.Suburb = dict["SUBURB"] as string;
+                            prop.Postcode = dict["POSTCODE"] as string;
+                            prop.AddressUseType = dict["ADDRESS USE TYPE"] as string;
+                            prop.WardName = dict["WARD NAME"] as string;
+                            prop.PropertyDescription = dict["PROPERTY DESCRIPTION"] as string;
+
+                            prop.BuildKey();
+
+                            spider.Search(prop);
+
+                            File.WriteAllText(lineNumberPath, index.ToString());
+                        }
+                        index += 1;
+
+                    }
+                }
+
+
+            }
+
+        }
+    }
+}
---
+++ HousePriceScraper/RealEstate.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using HtmlAgilityPack;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace HousePriceScraper
+{
+    public static class RealEstate
+    {
+        public static string baseUrl = @"https://www.realestate.com.au/property/";
+
+
+        public static SearchResult SearchRealEstate(Property property)
+        {
+            var address = property.BuildRealEstateAddress();
+
+            return SearchRealEstate(address);
+        }
+
+        public static SearchResult SearchRealEstate(string path)
+        {
+            var url = $@"{baseUrl}{path}";
+
+            SearchResult result = new SearchResult();
+
+            var cookieContainer = new CookieContainer();
+            using (var handler = new HttpClientHandler() { CookieContainer = cookieContainer })
+            {
+                using (HttpClient baseHttpClient = new HttpClient(handler))
+                {
+
+                    baseHttpClient.BaseAddress = new Uri(@"https://www.realestate.com.au/property/");
+
+                    baseHttpClient.DefaultRequestHeaders.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36");
+                    baseHttpClient.DefaultRequestHeaders.Add("upgrade-insecure-requests", "1");
+                    baseHttpClient.DefaultRequestHeaders.Add("cookie", "reauid=06083e1750520000a61b015ba40100008a2f3b00; KP_UID=c3ce491275ecbe41813e3f46cc8275c5");
+
+                    var response = baseHttpClient.GetAsync(path).GetAwaiter().GetResult();
+                    var html = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+                    response = baseHttpClient.GetAsync(path).GetAwaiter().GetResult();
+                    html = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+                    HtmlDocument document = new HtmlDocument();
+                    document.LoadHtml(html);
+
+                    // Property Value and Property Market Data - realestate.com.au
+
+                    var title = document.DocumentNode.DescendantsAndSelf().Where(n => n.Name == "title").FirstOrDefault();
+
+                    if (title != null && title.InnerText == "Property Value and Property Market Data - realestate.com.au")
+                    {
+                        result.InvalidAddress = true;
+                        return result;
+                    }
+
+                    var property_info = document.DocumentNode.DescendantsAndSelf()
+                        .Where(n => n.Name.ToLower() == "div" && n.HasClass("property-info__attributes"))
+                        .FirstOrDefault();
+
+                    if (property_info != null)
+                    {
+                        var bedrooms = property_info.DescendantsAndSelf()
+                            .Where(n => n.HasClass("rui-property-feature") && n.DescendantsAndSelf().Any(c => c.InnerText == "Bedrooms"))
+                            .FirstOrDefault();
+                        if (bedrooms != null)
+                        {
+                            var num = bedrooms.DescendantsAndSelf().Where(n => n.HasClass("config-num")).FirstOrDefault();
+                            result.Bedroom = num.InnerText;
+                        }
+
+                        var bathrooms = property_info.DescendantsAndSelf()
+                            .Where(n => n.HasClass("rui-property-feature") && n.DescendantsAndSelf().Any(c => c.InnerText == "Bathrooms"))
+                            .FirstOrDefault();
+                        if (bathrooms != null)
+                        {
+                            var num = bathrooms.DescendantsAndSelf().Where(n => n.HasClass("config-num")).FirstOrDefault();
+                            result.Bathroom = num.InnerText;
+                        }
+
+                        var carspaces = property_info.DescendantsAndSelf()
+                            .Where(n => n.HasClass("rui-property-feature") && n.DescendantsAndSelf().Any(c => c.InnerText == "Car Spaces"))
+                            .FirstOrDefault();
+                        if (carspaces != null)
+                        {
+                            var num = carspaces.DescendantsAndSelf().Where(n => n.HasClass("config-num")).FirstOrDefault();
+                            result.Parking = num.InnerText;
+                        }
+                    }
+
+                    Regex rgxPrediction = new Regex(@"{""confidence"":""\w+"",""range"":{""text"":""(\$[\d,]+ *- *\$[\d,]+)""}}");
+
+                    var predictionMatch = rgxPrediction.Match(html);
+                    if (predictionMatch.Success)
+                    {
+                        result.ValueRange = predictionMatch.Groups[1].Value;
+                    }
+
+                    Regex rgxPropertyId = new Regex(@"\/property\/purchase_title\/(\d+)");
+
+                    var propertyIdMatch = rgxPropertyId.Match(html);
+                    if (predictionMatch.Success)
+                    {
+                        // query the history
+                        string propertyId = propertyIdMatch.Groups[1].Value;
+
+                        string baseQueryUrl = @"https://pexa.realestate.com.au/graphql?query={property(propertyId:%22" +
+                            propertyId +
+                            "%22){id,propertyTimeline{date,agency,eventType,price}}}";
+
+                        using (HttpClient httpClient = new HttpClient())
+                        {
+                            httpClient.DefaultRequestHeaders.Referrer = new Uri(url);
+                            httpClient.DefaultRequestHeaders.Add("user-agent", " Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36");
+
+                            var jsonHistory = httpClient.GetStringAsync(baseQueryUrl).GetAwaiter().GetResult();
+
+                            // use http://json2csharp.com/ to generate the C# object from json
+                            var history = JsonConvert.DeserializeObject<HistoryObject>(jsonHistory);
+
+                            var timeline = history?.data?.property?.propertyTimeline;
+
+                            if (timeline != null)
+                            {
+                                result.Data = timeline.Select(ev =>
+                                {
+                                    return new HouseHistory()
+                                    {
+                                        Action = ev.eventType,
+                                        Date = ev.date,
+                                        Value = ev.price,
+                                        Agent = ev.agency
+                                    };
+                                }).ToList();
+                            }
+                        }
+                    }
+
+                    var propertyDetails = document.DocumentNode.DescendantsAndSelf()
+                        .Where(
+                        n => n.Name == "div" &&
+                        n.HasClass("property-details") &&
+                        n.DescendantsAndSelf().Any(
+                            t =>
+                            t.Name == "table" && t.HasClass("info-table")
+                            )).FirstOrDefault();
+
+                    if (propertyDetails != null)
+                    {
+                        var propertyInfoTable = propertyDetails.DescendantsAndSelf().Where(n => n.Name == "table" && n.HasClass("info-table")).FirstOrDefault();
+                        if (propertyInfoTable != null)
+                        {
+                            var propertyInfoTableBody = propertyInfoTable.DescendantsAndSelf().Where(n => n.Name == "tbody").FirstOrDefault();
+                            if (propertyInfoTableBody != null)
+                            {
+                                var propertyInfoRows = propertyInfoTableBody.DescendantsAndSelf().Where(n => n.Name == "tr").ToList();
+
+                                foreach (var infoRow in propertyInfoRows)
+                                {
+                                    var label = infoRow.Descendants().First(n => n.Name == "td").InnerText.ToLower();
+                                    var value = infoRow.Descendants().Last(n => n.Name == "td").InnerText;
+                                    switch (label)
+                                    {
+                                        case "land size":
+                                            result.LandSize = value;
+                                            break;
+                                        case "floor area":
+                                            result.FloorArea = value;
+                                            break;
+                                        case "year built":
+                                            result.YearBuilt = value;
+                                            break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            return result;
+        }
+    }
+
+
+    // the following data is used to deserialize the json from realestate;
+    public class PropertyTimeline
+    {
+        public string date { get; set; }
+        public string agency { get; set; }
+        public string eventType { get; set; }
+        public string price { get; set; }
+    }
+
+    public class HistoryProperty
+    {
+        public string id { get; set; }
+        public List<PropertyTimeline> propertyTimeline { get; set; }
+    }
+
+    public class HistoryData
+    {
+        public HistoryProperty property { get; set; }
+    }
+
+    public class HistoryObject
+    {
+        public HistoryData data { get; set; }
+    }
+}
